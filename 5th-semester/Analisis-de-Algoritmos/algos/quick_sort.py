204d41fc48d0e6ea4155c785f913cf5c172d7daa
aruzdh <aruzdh@gmail.com>
2025-09-03 09:53:19 -0600

    chore: Improve Analisis de Algoritmos notes (improve quick sort algo)

--- 5th-semester/Analisis-de-Algoritmos/algos/quick_sort.py
+++ 5th-semester/Analisis-de-Algoritmos/algos/quick_sort.py
@@ -14,29 +14,21 @@ def qsort(A):
 def handle(A, b, e):
     if e - b < 2:
         return
+
     ptr = e - 1
-    curr = 0
-    nxt = curr
+    center = b
 
-    while curr < ptr and nxt < ptr:
-        A[curr], A[nxt] = A[nxt], A[curr]
-        nxt += 1
+    for i in range(b, e - 1):
+        if A[i] <= A[ptr]:
+            A[center], A[i] = A[i], A[center]
+            center += 1
 
-        if A[curr] <= A[ptr]:
-            curr += 1
-        else:
-            # if nxt == ptr:
-            #     break
-            A[curr], A[nxt] = A[nxt], A[curr]
-            nxt += 1
-    print(curr, nxt + 1)
-    handle(A, 0, curr + 1)
-    handle(A, curr + 1, e)
+    A[center], A[ptr] = A[ptr], A[center]
+    handle(A, b, center)
+    handle(A, center, e)
 
 
-# array = [8, 7, 6, 510, 8, 6, 5, 4, 7, 6, 5, 54]
-# array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-array = [3, 1, 2]
+array = [8, 7, 6, 510, 8, 6, 5, 4, 7, 6, 5, 54]
 
 print(array)
 qsort(array)
